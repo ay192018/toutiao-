2992a303aa06958ae5b4c2b5826199970e624ba7
wangjunyu <2510186180@qq.com>
2021-10-20 18:25:16 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <router-view />
     <h1>头条</h1>
+    <div>
+      <i class="toutiao icon-wode"></i>
+      <van-button loading type="info" loading-text="Loading..." />
+    </div>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+//加载vant组件库
+import 'vant/lib/index.css'
+//注册组件库
+Vue.use(Vant);
 //加载全局样式
 import "./styles/index.less"
+// import './styles/icon.less'
 Vue.config.productionTip = false
 
 new Vue({
